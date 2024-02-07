69381f60029553d8e2c1201cd7b87fbabf9901b3
kjsaj0525 <kjsaj0525@gmail.com>
2024-02-08 01:44:51 +0900

    프로젝트 기초 공사 1

--- src/App.js
+++ src/App.js
@@ -6,11 +6,23 @@ import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import RouteTest from "./components/RouteTest";
 
+// COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
 function App() {
   return (
       <BrowserRouter>
           <div className="App">
               <h2>App.js</h2>
+              <MyHeader headText={'김진석 감정 일기'}
+                        leftChild={<MyButton text={'왼쪽버튼'} onClick={() => alert("왼쪽 클릭")} />}
+                        rightChild={<MyButton text={'오른쪽버튼'} onClick={() => alert("오른쪽 클릭")} />}
+              />
+
+              <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} type={"positive"} />
+              <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} type={"negative"} />
+              <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
               <Routes>
                   <Route path='/' element={<Home/>}/>
                   <Route path='/new' element={<New/>}/>
