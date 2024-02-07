5a0dbd8a3362247874c452e5419590eb9a878cae
kjsaj0525 <kjsaj0525@gmail.com>
2024-02-08 00:57:48 +0900

    페이지 라우팅 2 - React Router 응용

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,25 @@
+import {useNavigate, useSearchParams} from "react-router-dom";
+
 const Edit = () => {
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get("id");
+    const mode = searchParams.get("mode");
+
     return (
         <div>
             <h1>Home</h1>
             <p>이곳은 수정페이지 입니다</p>
+            <button onClick={() => setSearchParams({who: 'jinseok'})}>QS바꾸기</button>
+            <button onClick={() => {
+                navigate("/home");
+            }}>HOME으로가기
+            </button>
+            <button onClick={() => {
+                navigate(-1);
+            }}>뒤로가기
+            </button>
         </div>
     );
 };
